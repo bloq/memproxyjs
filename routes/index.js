de66aa125ce27eb3e260bb82cd58f4393cfe31d4
Martin Bon Foster <martin.bonfoster@bloq.com>
2020-03-10 15:08:02 -0300

    Fixs all auto-fixable lint problems

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,28 @@
-
-'use strict';
+'use strict'
 
 const pjson = require('../package.json')
 
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // GET server identity
 router.get('/', function(req, res, next) {
   res.json({
-      "name": pjson.name,
-      "version": pjson.version,
-  });
-});
+    name: pjson.name,
+    version: pjson.version
+  })
+})
 
 // memcache stats; also serves as a health check
 router.get('/stats', function(req, res) {
-  var memcached = req.app.locals.memcached;
+  const memcached = req.app.locals.memcached
   memcached.stats(function(err, results) {
     if (err) {
-      res.status(500).json(jerr.InternalServer);
+      res.status(500).json(jerr.InternalServer)
     } else {
-      res.json(results);
+      res.json(results)
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
